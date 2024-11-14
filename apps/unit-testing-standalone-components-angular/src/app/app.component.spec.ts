a5ebceb9cfd03219b6a1d96536fedda2a5e1df7f
Daves Macbook <el-davo@Davess-MacBook-Pro.local>
2024-11-14 19:09:18 +0000

    derfer

--- apps/unit-testing-standalone-components-angular/src/app/app.component.spec.ts
+++ apps/unit-testing-standalone-components-angular/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, DeferBlockState, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -27,7 +27,20 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
   })
 
-  it('should have a list of todos', () => {
+  it('should have a list of todos', async () => {
+    const deferBlock = (await fixture.getDeferBlocks())[0];
+
+    await deferBlock.render(DeferBlockState.Complete);
+
     expect(fixture.debugElement.query(By.css('app-todo-list'))).not.toBeNull();
+  });
+
+  it('should show a place holder when loading list of todos', async () => {
+    const deferBlock = (await fixture.getDeferBlocks())[0];
+
+    await deferBlock.render(DeferBlockState.Placeholder);
+
+    expect(fixture.debugElement.query(By.css('app-todo-list'))).toBeNull();
+    expect(fixture.debugElement.query(By.css('p')).nativeElement.textContent).toContain('Loading list');
   });
 });
